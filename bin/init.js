3d42c3b3e044c4794a24cd1ed41594c53a8a13c3
entom <tomasz@entom.info>
2022-08-07 16:10:08 +0200

    feat(app): handle file missing in init method, generate SECRET_KEY

--- bin/init.js
+++ bin/init.js
@@ -3,14 +3,22 @@ const crypter = require('../crypter');
 
 const FILE_PATH = `${__dirname}/../.env`;
 
+const writeFileContent = (content) => {
+  fs.writeFile(FILE_PATH, content, 'utf8', function (err) {
+    if (err) console.log(err);
+  });
+}
+
 fs.readFile(FILE_PATH, {encoding: 'utf8'}, function (err, data) {
-  if (err) throw err;
-  if (data.indexOf('SECRET_KEY=') === -1) {
-    data += '\nSECRET_KEY=' + crypter.generateRandomKey();
-    fs.writeFile(FILE_PATH, data, 'utf8', function (err) {
-      if (err) console.log(err);
-    });
+  if (err !== null) {
+    if (err.code === 'ENOENT') {
+      const data = 'SECRET_KEY=' + crypter.generateRandomKey();
+      writeFileContent(data);
+    }
+  } else {
+    if (data.indexOf('SECRET_KEY=') === -1) {
+      data += '\nSECRET_KEY=' + crypter.generateRandomKey();
+      writeFileContent(data);
+    }
   }
 });
-
-console.log('init -- end');
